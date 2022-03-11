20a815d2fc60c4476bedc2d52af7d4b88fa995c7
@sep0225 <sep0225@live.unc.edu>
2022-03-11 13:34:52 -0500

    removing process.exit(1)

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ fs.readFile("./www/index.html", "utf8", (err, data) => {
 if (err) {
     console.error(err)
     return
-    process.exit(1)
 }
 
 // Define a const `server` as an arrow function using http.createServer. 
